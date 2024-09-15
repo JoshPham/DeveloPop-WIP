15c025fbdb81e8893677f27aba716b035e3d2fbf
Josh Pham <JTPham911@gmail.com>
2024-09-14 20:07:29 -0700

    small UI changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   const educreateLink = process.env.NODE_ENV === 'production' ? 'https://educreate.developop.org' : '/educreate';
 
   return (
-    <>
+    <div className="w-screen overflow-x-hidden">
       <nav className="fixed z-40 w-screen h-20">
               <ul className="flex w-screen h-full justify-around items-center list-none">
                   <li><a href="/" className="text-3xl flex items-center">Develo<p className='font-sans font-bold'>Pop</p></a></li>
@@ -15,19 +15,19 @@ export default function Home() {
               </ul>
           </nav>
       <main className="flex min-h-screen flex-col items-center justify-between p-48">
-        <div className="animate-pulse absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(14,165,233,.4),rgba(255,255,255,0))]"></div>
-        <div className="animate-pulse absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(14,165,233,.4),rgba(255,255,255,0))]"></div>
-        <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:30px_40px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-50"></div>
+        <div className="max-w-screen animate-pulse absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(14,165,233,.4),rgba(255,255,255,0))]"></div>
+        <div className="max-w-screen animate-pulse absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(14,165,233,.4),rgba(255,255,255,0))]"></div>
+        <div className="max-w-screen absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:30px_40px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-50"></div>
         
-        <div className='flex flex-col justify-center items-center gap-8 w-[60%] text-center'>
-            <h1 className="text-5xl font-bold z-10">Welcome to Developop</h1>
-            <p className="text-xl">
+        <div className='flex flex-col justify-center items-center gap-8 w-screen lg:w-[80%] text-center'>
+            <h1 className="text-4xl md:text-5xl font-bold z-10 sm:leading-[3rem] md:leading-[4rem]">Welcome to Developop</h1>
+            <p className="text-xl p-10">
               The Developop Team is currently working on the new website, so please be patient as we launch a new 
               and improved website.
             </p>
         </div>
 
       </main>
-    </>
+    </div>
   );
 }
